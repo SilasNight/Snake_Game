761c997f19c57a69c551d5d9b61f28dea4753b77
SilasNight <15emiliomurray@gmail.com>
2026-04-08 11:19:47 +0200

    Adding planning.txt abd made a main window to house the game

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Let's make Snake
+import tkinter as tk
+
+
+class Game:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Snake Game - Python")
+        self.window.geometry("800x800")
+        self.window.config(bg="white")
+
+        # Setup main Menu
+
+
+        self.window.mainloop()
+
+
+if __name__ == "__main__":
+    Game()
