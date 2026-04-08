489169a23be48badc36920e21856a6f79b442588
SilasNight <15emiliomurray@gmail.com>
2026-04-08 16:01:30 +0200

    Added a border and trying to figure out how I am going to make it move...

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ class Game:
         self.window = tk.Tk()
         self.window.title("Snake Game - Python")
         self.window.geometry("800x800")
+        self.window.resizable(False, False)
         self.window.config(bg="white")
 
         # Setup main Menu
+        # self.settings_canvas = tk.Canvas(self.window)
+
+        self.tail_length = 4
+        self.game_window = tk.Canvas(self.window, bg="white", height=800, width=800)
+        self.game_window.place(x=-1, y=-1)
+
+        self.game_window.create_rectangle(5, 5, 795, 795, width=10)
 
 
         self.window.mainloop()
